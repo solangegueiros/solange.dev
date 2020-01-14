edd2cb27e797752ecd6bdf4938e0bec28d06413f
Solange Gueiros <solangegueiros@gmail.com>
2020-01-14 00:54:01 -0300

    talks on main page

--- src/components/header.js
+++ src/components/header.js
@@ -31,9 +31,7 @@ const Header = () => {
           <h3 style={{ display: `inline` }}>{data.site.siteMetadata.title}</h3>
         </Link>        
         <ul style={{ listStyle: `none`, float: `right` }}>
-          <ListLink to="/">Home</ListLink>
-          <ListLink to="/about/">About</ListLink>
-          <ListLink to="/talks/">Talks</ListLink>
+          <ListLink to="/">Home - inicio</ListLink>
         </ul>
       </header>
     </>
@@ -41,6 +39,8 @@ const Header = () => {
 } 
 
 /*
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/talks/">Talks</ListLink>
           <ListLink to="/blog/">Blog</ListLink>
 */
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,68 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 
-export default () => (
-  <Layout>
-    <h1>Blockchain Developers Blog</h1>
-    <p>
-      This is my Blockchain Developers Blog.
-      <br/>
-      Solange Gueiros
-    </p>
-  </Layout>
-)
+export default ({ data }) => {
+  //console.log(data)
+  return (
+    <Layout>
+      <div>
+        <p>
+          This is my Blockchain Developers Blog.  
+          For now I put all talks and interviews from 2019.  
+          Some things are um Portuguese, others are in English.
+          <br/>
+          Este é meu blog para desenvolvedores Blockchain. 
+          Por enquanto eu coloquei apenas minhas palestras e entrevistas de 2019. 
+          Algumas coisas estão em português, outras em inglês.
+          <br/>
+          Sol - Solange Gueiros
+        </p>
+        <h3>
+          Palestras e entrevistas - Talks and interviews
+        </h3>
+        <h4>{data.allMarkdownRemark.totalCount} Talks</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link to={node.fields.slug}>
+              <h3> {node.frontmatter.title}{" "}</h3>
+            </Link>
+            <p>
+              <span>
+                {node.frontmatter.date} - {node.frontmatter.description}
+              </span>
+            </p>
+            <br/>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: {frontmatter: {type: {eq: "talk"}}}
+      ) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD/MMM/YYYY")
+            description
+          }
+          fields {
+            slug
+          }          
+          timeToRead
+          excerpt
+        }
+      }
+    }
+  }
+`
