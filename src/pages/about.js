d0a96adc029ac8590e91148137227717a85c224b
Solange Gueiros <solangegueiros@gmail.com>
2022-09-15 13:23:40 -0300

    events-2022-09-15

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,25 +13,40 @@ const About = ({ pageContext }) => {
   return (
     <Layout pageContext={pageContext} pageTitle={t("about")} >
       <Seo title={t("about")} />
+      <p>
+        <b>Sol - Solange Gueiros</b>        
+      </p>
+      <br/>      
       <StaticImage width={200}
         alt="Solange Gueiros"
         src="../images/SolangeGueiros-2021-12-Rosto.jpg"
       />
       <h3>Mini bio</h3>
       <p>
+        <b>{t("miniBioTitle")}</b>
+        <br/>
+      </p>
+      <p>
         {t("miniBioContent")}
         <br/>
       </p>
+      <p>
+        <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/solangegueiros" className={navLinkText}>
+          Twitter
+        </a>        
+        <br/>
+      </p>
+      <p>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/solangegueiros/" className={navLinkText}>
+          Linkedin
+        </a>        
+        <br/>
+      </p>      
       <h3>
         <a target="_blank" rel="noopener noreferrer" href="https://photos.app.goo.gl/S64866GpCHbvhJk86" className={navLinkText}>
           {t("profilePictures")}
         </a>
       </h3>
-      <p>
-        <br/><br/>
-        Sol - Solange Gueiros
-      </p>
-      <br/>
     </Layout>
   )
 }
