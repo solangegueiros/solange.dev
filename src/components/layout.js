81d1233ec8a3b65ac064945158fc15341ba48cc3
Solange Gueiros <solangegueiros@gmail.com>
2025-04-20 23:42:06 -0300

    events 2025-04-20

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,35 +1,25 @@
 import * as React from 'react'
 import { MdxLink, LocalizedLink as Link, useLocalization } from "gatsby-theme-i18n"
+import Menu from "../components/Menu"
 import Language from "../components/language"
 import { useStaticQuery, graphql } from 'gatsby'
 import {
   container,
   heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
+  navWrapper,
   siteTitle,  
+  logoLink,
+  logoIcon,
 } from '../styles/layout.module.css'
+import icon from '../images/icon.png'
+
 
 const components = {
   a: MdxLink,
 }
 
 const Layout = ({ pageTitle, children, pageContext }) => {
-/*
-  const { locale } = useLocalization()
 
-          <li className={navLinkItem}>
-            <Link to="/blog" className={navLinkText}>
-              Blog
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/locales" className={navLinkText}>
-              Locales info {locale}
-            </Link>
-          </li>
-*/  
 
   const data = useStaticQuery(graphql`
     query {
@@ -46,40 +36,16 @@ const Layout = ({ pageTitle, children, pageContext }) => {
     <div >
       <title>{pageTitle} | {data.site.siteMetadata.title}</title>
       <header className={siteTitle}>
-        <Link to="/">
+        <Link to="/" className={logoLink}>
+          <img src={icon} alt="Site icon" className={logoIcon} />
           {data.site.siteMetadata.title}
         </Link>        
-      </header>
-      <nav>
-        <ul className={navLinks}>
-        <li className={navLinkItem}>
-            <Link to="/events" className={navLinkText}>
-              Events
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <a target="_blank" rel="noopener noreferrer" href="https://ethereum.solange.dev/" className={navLinkText}>
-              Ethereum
-            </a>
-          </li>
-          <li className={navLinkItem}>
-            <a target="_blank" rel="noopener noreferrer" href="https://academy.rsk.dev.br/" className={navLinkText}>
-              RSK Academy
-            </a>
-          </li>
-          <li className={navLinkItem}>
-            <a target="_blank" rel="noopener noreferrer" href="https://rsk.solange.dev/" className={navLinkText}>
-              RSK
-            </a>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <Language pageContext={pageContext}/>
+      </header>      
+
+      <div className={navWrapper}>
+        <Menu pageContext={pageContext}/>
+        <Language pageContext={pageContext}/>
+      </div>      
 
       <main>
         <div className={container}>
